6b502f88c7934204dfe5bfa81be027086728c592
Jai Morjaria <jaimorjaria2099@gmail.com>
2025-10-13 17:55:26 -0400

    merging script for every season

--- master_csv_creator.py
+++ master_csv_creator.py
@@ -2,29 +2,46 @@ import pandas as pd
 import os
 from pathlib import Path
 
-def combine_csv_files(file_list, output_file):    
-    master_df = pd.DataFrame(pd.read_csv(Path("CTG_CSV_Data\\2003-04") / file_list[0]))
+def combine_csv_files(dir_path, output_file):    
+    file_list = [f for f in os.listdir(dir_path) if f.endswith('_Regular_Season.csv')]
+    first_file_path = Path(dir_path) / file_list[0]
+    master_df = pd.DataFrame(pd.read_csv(first_file_path))
+    common_keys = ['Player', 'Team', 'Age', 'Pos', 'MIN']
+    shooting_common_keys = ['eFG%', 'eFG% Rank']
+
 
     for i in range(1, len(file_list)):
         file = file_list[i]
         file_path = Path("CTG_CSV_Data\\2003-04") / file
         df = pd.read_csv(file_path)
-        if "_Team_" in file:
-            df = df.add_prefix('Team_')
-        elif "_Opponent_" in file:
-            df = df.add_prefix('Opp_')
-        df = df.drop(columns=[col for col in df.columns if col in master_df.columns])
+        df = df.drop(columns=common_keys)
+        for key in shooting_common_keys:
+            if key in df.columns and key in master_df.columns:
+                df = df.drop(columns=key)
+        if "OnOff" in file:
+            if "Team" in file:
+                df = df.add_prefix(f"OnOff_Team_")
+            else:
+                df = df.add_prefix(f"OnOff_Opp_")
+        else:
+            if "Shooting" in file:
+                shooting_category = file.split('_')[1]
+                df = df.rename(columns={
+                    col: f"{col}_{shooting_category}" if col != "eFG%" and col != "eFG% Rank" else col for col in df.columns
+                })
         master_df = pd.concat([master_df, df], axis=1)
 
 
-    # duplicate_cols = master_df.columns[master_df.columns.duplicated()]
-    # print(f"Duplicate columns found: {duplicate_cols.tolist()}")
-    # print(master_df.columns.tolist())
-    # print(len(master_df.columns.tolist()))
-    # master_df = master_df.drop(columns=duplicate_cols)
     print(f"Total columns in master dataframe: {len(master_df.columns)}")    
-    master_df.to_csv(output_file, index=False)
-                 
-files_to_combine = [file for file in os.listdir("CTG_CSV_Data\\2003-04") if file.endswith('Regular_Season.csv')]
-print(files_to_combine)
-combine_csv_files(files_to_combine, "merged.csv")
+    master_df.to_csv(f"merged_csvs/{output_file}", index=False)
+
+
+for year in range(2003, 2024):
+    season = f"{year}-{str(year+1)[-2:]}"
+    dir_path = Path(f"CTG_CSV_Data\\{season}")
+    if not dir_path.exists():
+        print(f"Directory for season {season} does not exist. Skipping...")
+        continue
+    output_filename = f"merged_{season}.csv"
+    combine_csv_files(dir_path, output_filename)
+    print(f"Combined CSV for season {season} saved as {output_filename}")
